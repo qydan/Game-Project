b9d8e70075d18f01ac60701094ac10655edae1b9
qydan <94135697+qydan@users.noreply.github.com>
2023-05-09 00:34:49 -0400

    Update WordleGame.java
    integeration of frontend and backend

--- WordleGame.java
+++ WordleGame.java
@@ -1,19 +1,35 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 class WordleGame extends JFrame implements ActionListener {
     private JTextField inputField;
     private JButton guessButton;
-    private JLabel resultLabel, wordLabel;
+    //private JLabel resultLabel, wordLabel;
     private JPanel inputPanel, resultPanel, wordPanel;
+
+    private int currentGuess;
+
+    private List<List<JLabel>> labelList = new ArrayList<>();
+    private Board gameBoard;
 
     public WordleGame() {
 
         setTitle("Wordle Game");
-        setSize(400, 300);
+        setSize(400, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
+
+        gameBoard = new Board();
 
         // create components
         inputField = new JTextField();
@@ -26,27 +42,102 @@ class WordleGame extends JFrame implements ActionListener {
         inputPanel.add(inputField, BorderLayout.NORTH);
         inputPanel.add(guessButton, BorderLayout.CENTER);
 
-        resultLabel = new JLabel();
+        //resultLabel = new JLabel();
         resultPanel = new JPanel();
-        resultPanel.add(resultLabel);
+        resultPanel.setLayout(null);
+        resultPanel.setBackground(Color.darkGray);
 
-        wordLabel = new JLabel();
-        wordPanel = new JPanel();
-        wordPanel.add(wordLabel);
+        initLabels();
 
+        //resultPanel.add(resultLabel);
+
+        //wordLabel = new JLabel();
+        //wordPanel = new JPanel();
+        //wordPanel.set
+        //wordPanel.add(wordLabel);
+        //resultPanel.setBackground(Color.RED);
         // add panels to frame
         add(inputPanel, BorderLayout.NORTH);
         add(resultPanel, BorderLayout.CENTER);
-        add(wordPanel, BorderLayout.SOUTH);
+        //add(wordPanel, BorderLayout.SOUTH);
 
         // add action listener to guess button
 
         setVisible(true);
     }
 
-    public void actionPerformed(ActionEvent e) {
-        // Back end to be implemented in another class by Uros.
-    }
-    
-}
+    private void initLabels() {
 
+        for (int rows = 0; rows < 6; rows++) {
+
+            List<JLabel> tempLabels = new ArrayList<>();
+
+            for (int cols = 0; cols < 5; cols++) {
+
+                int width = getWidth() / 6;
+
+                JLabel label = new JLabel();
+                label.setForeground(Color.WHITE);
+                label.setBounds(width * cols + width, width * rows + width, width, width);
+                label.setText("[ ]");
+                tempLabels.add(label);
+                resultPanel.add(label);
+
+
+            }
+
+            this.labelList.add(tempLabels);
+        }
+
+        validate();
+        repaint();
+
+    }
+
+    public boolean validateText(String t) {
+
+        if (t.length() != 5) return false;
+        //add more conditions if necessary
+
+        return true;
+    }
+
+    public void actionPerformed(ActionEvent e) {
+
+
+        if (e.getSource() == guessButton) {
+
+            String textIn = inputField.getText().toLowerCase();
+
+            if (!validateText(textIn)) return;
+
+            inputField.setText("");
+
+            char[] letters = textIn.toCharArray();
+            List<JLabel> activeLabels = labelList.get(currentGuess);
+
+            for (int x = 0; x < activeLabels.size(); x++) {
+
+                char l = letters[x];
+                JLabel label = activeLabels.get(x);
+                label.setForeground(gameBoard.getColorAt(textIn, x));
+                label.setText("" + l);
+
+
+            }
+
+            /* this is the condition for if the user has guessed correctly to be implemented
+            if (textIn.equals("hidden word")){
+                dispose();
+                JOptionPane.showMessageDialog(this, "YOU WON!","CONGRATULATIONS", JOptionPane.INFORMATION_MESSAGE);
+                setVisible(true);
+            }
+            */
+
+
+            currentGuess++;
+        }
+
+    }
+
+}
